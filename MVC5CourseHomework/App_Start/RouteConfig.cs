9ed17f70ab20b76213fc6b74657c64347831ebd3
Chung-Nan Lin <lin.jacky@gmail.com>
2018-07-11 20:15:26 +0800

    請實作出「客戶資料管理」、「客戶聯絡人管理」與「客戶銀行帳戶管理」等簡易 CRUD 功能

--- MVC5CourseHomework/App_Start/RouteConfig.cs
+++ MVC5CourseHomework/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC5CourseHomework
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "客戶資料", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
